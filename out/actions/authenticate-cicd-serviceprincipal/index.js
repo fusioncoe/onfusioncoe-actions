a544f57c139fe83fffa201e412c38bf72a7b068a
Ryan Millett <ryan.millett@74bravo.com>
2025-09-18 22:00:15 -0400

    testing authentication

--- out/actions/authenticate-cicd-serviceprincipal/index.js
+++ out/actions/authenticate-cicd-serviceprincipal/index.js
@@ -10,18 +10,32 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const core = require("@actions/core");
-//import { runnerParameters } from '../../lib/runnerParameters';
+const node_fetch_1 = require("node-fetch");
 (() => __awaiter(void 0, void 0, void 0, function* () {
     core.startGroup('authenticate-cicd-serviceprincipal:');
     const cred = {
         client_id: core.getInput('client_id'),
         client_secret: core.getInput('client_secret'),
-        tenant_id: core.getInput('tenant_id'),
         grant_type: 'client_credentials',
         scope: core.getInput('scope')
     };
+    const tenant_id = core.getInput('tenant_id');
+    const fetchUrl = `https://login.microsoftonline.com/${tenant_id}}/oauth2/v2.0/token`;
+    const response = yield (0, node_fetch_1.default)(fetchUrl, {
+        method: 'POST',
+        body: JSON.stringify(cred),
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+        },
+    });
+    if (!response.ok) {
+        throw new Error(`Error! status: ${response.status}`);
+    }
+    var authResponse = (yield response.json());
+    const bearer = `bearer ${authResponse.access_token}`;
     //core.setOutput('SPN_BEARER', 'SPN BEARER VALUE');
-    core.exportVariable('SPN_BEARER', 'SPN BEARER VALUE');
+    core.exportVariable('SPN_BEARER', bearer);
     core.endGroup();
 }))().catch(error => {
     //const logger = runnerParameters.logger;
--- src/actions/authenticate-cicd-serviceprincipal/index.ts
+++ src/actions/authenticate-cicd-serviceprincipal/index.ts
@@ -1,9 +1,12 @@
 import * as core from '@actions/core';
+import fetch from 'node-fetch';
 
 
 //import { runnerParameters } from '../../lib/runnerParameters';
 
-
+type authResponse = {
+  access_token: string;
+};
 
 
 (async() => {
@@ -11,12 +14,32 @@ import * as core from '@actions/core';
     const cred = {
         client_id: core.getInput('client_id'),
         client_secret: core.getInput('client_secret'),
-        tenant_id: core.getInput('tenant_id'),        
         grant_type: 'client_credentials',
         scope: core.getInput('scope')
     };
+
+    const tenant_id = core.getInput('tenant_id');
+    const fetchUrl = `https://login.microsoftonline.com/${tenant_id}}/oauth2/v2.0/token`
+
+    const response = await fetch(fetchUrl, {
+      method: 'POST',
+      body: JSON.stringify(cred),
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error! status: ${response.status}`);
+    }
+
+    var authResponse = (await response.json()) as authResponse;
+
+    const bearer = `bearer ${authResponse.access_token}`;
+
     //core.setOutput('SPN_BEARER', 'SPN BEARER VALUE');
-    core.exportVariable('SPN_BEARER', 'SPN BEARER VALUE');
+    core.exportVariable('SPN_BEARER', bearer);
     core.endGroup();
 })().catch(error => {
     //const logger = runnerParameters.logger;
