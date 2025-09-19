3532c53aae5bdb007f3b315d261b6fffc9fafdd5
Ryan Millett <ryan.millett@74bravo.com>
2025-09-18 21:00:42 -0400

    checking in merges

---
+++ src/actions/authenticate-cicd-serviceprincipal/index.ts
@@ -0,0 +1,24 @@
+import * as core from '@actions/core';
+
+
+//import { runnerParameters } from '../../lib/runnerParameters';
+
+
+
+
+(async() => {
+    core.startGroup('authenticate-cicd-serviceprincipal:');
+    const cred = {
+        client_id: core.getInput('client_id'),
+        client_secret: core.getInput('client_secret'),
+        tenant_id: core.getInput('tenant_id'),        
+        grant_type: 'client_credentials',
+        scope: core.getInput('scope')
+    };
+    core.setOutput('SPN_BEARER', 'SPN BEARER VALUE');
+    core.endGroup();
+})().catch(error => {
+    //const logger = runnerParameters.logger;
+    //logger.error(`failed: ${error}`);    
+    core.endGroup();
+});
